a5d95d18ec1ddfa1eb3c864a59dbbe4f79153574
gamboel99 <eko.w.a@students.kahuripan.ac.id>
2025-11-12 11:13:02 +0700

    Add files via upload

---
+++ codes.js
@@ -0,0 +1,15 @@
+// codes.js
+const CODES = [
+  "GSPR-NS40Z",
+  "GSPR-NS40ZL",
+  "GSPR-NS60",
+  "GSPR-NS60L",
+  "GSPR-NS60LS",
+  "GSPR-N70Z",
+  "GSPR-N100",
+  "AMRN-38B20L",
+  "YTZ5S",
+  "CHILWEE-6-DZF-12",
+  "AIR ACCU",
+  "AIR ZUUR"
+];
